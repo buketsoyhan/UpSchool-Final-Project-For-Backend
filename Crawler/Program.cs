c071d64c163a25732f2b5ab79fa8dc3aaba3e205
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-05-18 16:56:50 +0300

    Taken to the try catch blog.

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -6,87 +6,94 @@ class Program
 {
     static void Main(string[] args)
     {
-        IWebDriver driver = new ChromeDriver();
-
-        DateTime now = DateTime.Now;
-        Console.WriteLine("Website logged in. - " + now.ToString("dd.MM.yyyy : HH:mm"));
-        List<Product> products = new List<Product>();
-        string pattern = @"(\d+)";
-        string id;
-
-        for (int page = 1; page <= 10; page++)
+        try
         {
-            driver.Navigate().GoToUrl($"https://finalproject.dotnet.gg/?currentPage={page}");
+            IWebDriver driver = new ChromeDriver();
 
-            IReadOnlyCollection<IWebElement> productElements = driver.FindElements(By.CssSelector(".card"));
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Website logged in. - " + now.ToString("dd.MM.yyyy : HH:mm"));
+            List<Product> products = new List<Product>();
+            string pattern = @"(\d+)";
+            string orderId;
 
-            foreach (IWebElement productElement in productElements)
+            for (int page = 1; page <= 10; page++)
             {
-                string name = productElement.FindElement(By.CssSelector(".product-name")).Text;
-                string price = productElement.FindElement(By.CssSelector(".price")).Text;
-                string imageLocation = productElement.FindElement(By.CssSelector(".card-img-top")).GetAttribute("src");
+                driver.Navigate().GoToUrl($"https://finalproject.dotnet.gg/?currentPage={page}");
 
-                Match match = Regex.Match(imageLocation, pattern);
+                IReadOnlyCollection<IWebElement> productElements = driver.FindElements(By.CssSelector(".card"));
 
-                if (match.Success)
+                foreach (IWebElement productElement in productElements)
                 {
-                    id = match.Value;
+                    string name = productElement.FindElement(By.CssSelector(".product-name")).Text;
+                    string price = productElement.FindElement(By.CssSelector(".price")).Text;
+                    string picture = productElement.FindElement(By.CssSelector(".card-img-top")).GetAttribute("src");
+
+                    Match match = Regex.Match(picture, pattern);
+
+                    if (match.Success)
+                    {
+                        orderId = match.Value;
+                    }
+                    else
+                    {
+                        orderId = Guid.NewGuid().ToString();
+                    }
+
+                    bool isOnSale = productElement.FindElements(By.CssSelector(".onsale")).Count > 0;
+
+                    if (isOnSale == true)
+                    {
+                        string onSalePrice = productElement.FindElement(By.CssSelector(".sale-price")).Text;
+                        products.Add(new Product { Name = name, Price = price, Picture = picture, IsOnSale = isOnSale, OnSalePrice = onSalePrice, OrderId = orderId });
+
+                    }
+
+                    else
+                    {
+                        products.Add(new Product { Name = name, Price = price, Picture = picture, IsOnSale = isOnSale, OrderId = orderId });
+
+                    }
                 }
-                else
+
+                Console.WriteLine(($"Page {page} scanned. Total {products.Count} products - " + now.ToString("dd.MM.yyyy : HH:mm")));
+
+            }
+            var totalProduct = products.Count;
+            Console.WriteLine(($"{totalProduct} products detected - " + now.ToString("dd.MM.yyyy : HH:mm")));
+
+            driver.Quit();
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"Product Name: {product.Name}");
+                Console.WriteLine($"Product Price: {product.Price}");
+                Console.WriteLine($"Product Image: {product.Picture}");
+                Console.WriteLine($"Product Is On Sale: {product.IsOnSale}");
+                Console.WriteLine($"Product Id: {product.OrderId}");
+                if (product.IsOnSale == true)
                 {
-                    id= Guid.NewGuid().ToString();
+                    Console.WriteLine($"Product On Sale Price: {product.OnSalePrice}");
                 }
-
-                bool isOnSale = productElement.FindElements(By.CssSelector(".onsale")).Count > 0;
-
-                if(isOnSale==true)
-                {
-                    string onSalePrice = productElement.FindElement(By.CssSelector(".sale-price")).Text;
-                    products.Add(new Product { Name = name, Price = price, ImageLocation = imageLocation, IsOnSale = isOnSale, OnSalePrice = onSalePrice, Id=id });
-
-                }
-
-                else
-                {
-                    products.Add(new Product { Name = name, Price = price, ImageLocation = imageLocation, IsOnSale = isOnSale, Id = id });
-
-                }
+                Console.WriteLine("------------------------");
             }
 
-            Console.WriteLine(($"Page {page} scanned. Total {products.Count} products - " + now.ToString("dd.MM.yyyy : HH:mm")));
+            Console.WriteLine("Data scraping completed. - " + now.ToString("dd.MM.yyyy : HH:mm"));
 
+            Console.ReadLine();
         }
-        var totalProduct = products.Count;
-        Console.WriteLine(($"{totalProduct} products detected - " + now.ToString("dd.MM.yyyy : HH:mm")));
-
-        driver.Quit();
-
-        foreach (Product product in products)
+        catch (Exception ex)
         {
-            Console.WriteLine($"Product Name: {product.Name}");
-            Console.WriteLine($"Product Price: {product.Price}");
-            Console.WriteLine($"Product Image: {product.ImageLocation}");
-            Console.WriteLine($"Product Is On Sale: {product.IsOnSale}");
-            Console.WriteLine($"Product Id: {product.Id}");
-            if (product.IsOnSale == true )
-            {
-                Console.WriteLine($"Product On Sale Price: {product.OnSalePrice}");
-            }
-            Console.WriteLine("------------------------");
+            Console.WriteLine(ex.ToString());
         }
-
-        Console.WriteLine("Data scraping completed. - " + now.ToString("dd.MM.yyyy : HH:mm"));
-
-        Console.ReadLine();
     }
 }
 
 class Product
 {
-    public string Id { get; set; }
+    public string OrderId { get; set; }
     public string Name { get; set; }
     public string Price { get; set; }
-    public string ImageLocation { get; set; }
+    public string Picture { get; set; }
     public bool IsOnSale { get; set; }
     public string OnSalePrice { get;set; }
 }
